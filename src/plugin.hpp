a59bb7507f63d6fb95c6d807d117d65345cc0c5c
Danalizi-Erőss Örs <danalizieors@gmail.com>
2020-09-03 23:46:39 +0300

    Add plugin definition

--- src/plugin.hpp
+++ src/plugin.hpp
@@ -4,8 +4,7 @@
 
 using namespace rack;
 
-// Declare the Plugin, defined in plugin.cpp
+
 extern Plugin* pluginInstance;
 
-// Declare each Model, defined in each module source file
-// extern Model* modelMyModule;
+extern Model* modelSpringTheory;
