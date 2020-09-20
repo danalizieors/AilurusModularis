d4e3041e5519d4b24d0e3470b67d2dcdb2210dc4
Danalizi-Erőss Örs <danalizieors@gmail.com>
2020-09-20 22:22:45 +0300

    Change friction to attack time

--- src/SpringTheory.cpp
+++ src/SpringTheory.cpp
@@ -1,5 +1,14 @@
 #include "plugin.hpp"
 
+const float MINIMUM_TIME = 1e-3f;
+const float MAXIMUM_TIME = 10.f;
+const float TIME_RATIO = MAXIMUM_TIME / MINIMUM_TIME;
+
+const int NOTES = 12;
+const int CENTS = 100;
+const float UNNOTICEABLE_DIFFERENCE = 1.f / (NOTES * CENTS);
+const float LOG_UNNOTICEABLE_DIFFERENCE = log(UNNOTICEABLE_DIFFERENCE);
+
 Vec grid(float x, float y) {
 	float halfGrid = RACK_GRID_WIDTH * 0.5f;
 	return Vec(halfGrid * x, halfGrid * y);
@@ -9,13 +18,13 @@ struct SpringTheory : Module {
 	enum ParamIds {
 		OFFSET_PARAM,
 		FREQUENCY_PARAM,
-		FRICTION_PARAM,
+		ATTACK_PARAM,
 		POSITION_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds {
 		FREQUENCY_INPUT,
-		FRICTION_INPUT,
+		ATTACK_INPUT,
 		POSITION_INPUT,
 		RESET_INPUT,
 		NUM_INPUTS
@@ -35,8 +44,8 @@ struct SpringTheory : Module {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 
 		configParam(OFFSET_PARAM, 0.f, 1.f, 1.f, "Offset");
-		configParam(FREQUENCY_PARAM, -10.f, 10.f, 0.f, "Frequency", "Hz", 2, 2);
-		configParam(FRICTION_PARAM, 0.f, 10.f, 1.f, "Friction", "Ns/m");
+		configParam(FREQUENCY_PARAM, -5.f, 5.f, 0.f, "Frequency", "Hz", 2, 2);
+		configParam(ATTACK_PARAM, 0.f, 1.f, 0.5f, "Attack", "ms", TIME_RATIO, MINIMUM_TIME * 1000);
 		configParam(POSITION_PARAM, -5.f, 5.f, -5.f, "Position", "m");
 	}
 
@@ -56,9 +65,9 @@ struct SpringTheory : Module {
 		frequencyControl += inputs[FREQUENCY_INPUT].getVoltage();
 		frequencyControl = clamp(frequencyControl, -5.f, 5.f);
 
-		float friction = params[FRICTION_PARAM].getValue();
-		friction += inputs[FRICTION_INPUT].getVoltage();
-		friction = std::max(0.f, friction);
+		float attackParam = params[ATTACK_PARAM].getValue();
+		float attackInput = inputs[ATTACK_INPUT].getVoltage() * 0.1f;
+		float attack = (pow(TIME_RATIO, attackParam) + pow(TIME_RATIO, attackInput)) * MINIMUM_TIME;
 
 		float targetPosition = params[POSITION_PARAM].getValue();
 		targetPosition += offset;
@@ -73,6 +82,10 @@ struct SpringTheory : Module {
 		float frequency = 2.f * pow(2, frequencyControl);
 		float angularVelocity = 2 * M_PI * frequency;
 		float stiffness = pow(angularVelocity, 2);
+
+		float decay = -LOG_UNNOTICEABLE_DIFFERENCE / attack;
+		float dampingRatio = decay / angularVelocity;
+		float friction = 2 * sqrt(stiffness) * dampingRatio;
 
 		// simulate spring movement
 		float difference = position - targetPosition;
@@ -109,8 +122,8 @@ struct SpringTheoryWidget : ModuleWidget {
 
 		addInput(createInputCentered<PJ301MPort>(grid(3, 11), module, SpringTheory::FREQUENCY_INPUT));
 		addParam(createParamCentered<RoundBlackKnob>(grid(8, 11), module, SpringTheory::FREQUENCY_PARAM));
-		addInput(createInputCentered<PJ301MPort>(grid(3, 16), module, SpringTheory::FRICTION_INPUT));
-		addParam(createParamCentered<RoundBlackKnob>(grid(8, 16), module, SpringTheory::FRICTION_PARAM));
+		addInput(createInputCentered<PJ301MPort>(grid(3, 16), module, SpringTheory::ATTACK_INPUT));
+		addParam(createParamCentered<RoundBlackKnob>(grid(8, 16), module, SpringTheory::ATTACK_PARAM));
 
 		addParam(createLightParamCentered<LEDLightSlider<WhiteLight>>(grid(3, 34), module, SpringTheory::POSITION_PARAM, SpringTheory::DIFFERENCE_LIGHT));
 		addParam(createParamCentered<CKSS>(grid(6, 44), module, SpringTheory::OFFSET_PARAM));
